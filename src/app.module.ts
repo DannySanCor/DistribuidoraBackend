fecb7cc73a40336224464d721cc519fa80913ca2
DannySanCor <59484507+DannySanCor@users.noreply.github.com>
2021-09-02 17:55:19 -0500

    auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,17 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SociosModule } from './socios/socios.module';
 import { AppController } from './app.controller';
 import { SociosService } from './socios/socios.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [SociosModule,
     MongooseModule.forRoot('mongodb://localhost/distribuidora-nest',
     {
       useNewUrlParser:true
-    })],
+    }),
+    AuthModule,
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
